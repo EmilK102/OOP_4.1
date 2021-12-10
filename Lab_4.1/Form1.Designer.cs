0251f786b7972da3b50f178933c61f5c01f18c77
EmilK102 <vip.elick@gmail.com>
2021-12-11 00:25:30 +0500

    final

--- Lab_4.1/Form1.Designer.cs
+++ Lab_4.1/Form1.Designer.cs
@@ -29,13 +29,35 @@ namespace Lab_4._1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.paintBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.paintBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paintBox
+            // 
+            this.paintBox.Location = new System.Drawing.Point(12, 12);
+            this.paintBox.Name = "paintBox";
+            this.paintBox.Size = new System.Drawing.Size(643, 415);
+            this.paintBox.TabIndex = 0;
+            this.paintBox.TabStop = false;
+            this.paintBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paintBox_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.paintBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.paintBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox paintBox;
     }
 }
 
